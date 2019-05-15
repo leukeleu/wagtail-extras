edcf374162e1197865e3fc66e27e419e5e8b053c
Ramon de Jezus <rdejezus@leukeleu.nl>
2019-05-15 09:49:38 +0200

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtail-extras',
-    version='0.2.1',
+    version='0.2.2',
     description='A collection of tools one might use to make wagtail even better',
     long_description=io.open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
     keywords=['wagtail', 'extra',],
     author='Ramon de Jezus (Leukeleu)',
     author_email='rdejezus@leukeleu.nl',
